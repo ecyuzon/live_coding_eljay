ebe546938a10aa288e83da0af137790e3390b33a
Eljay Yuzon <ecyuzon@gmail.com>
2026-03-09 17:16:29 +0800

    General UI Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+document.addEventListener('DOMContentLoaded', function() {
+    window.incrementNotifications = function() {
+        var notificationCountElement = document.getElementById('notif_ctr');
+        var currentCount = parseInt(notificationCountElement.textContent);
+        notificationCountElement.textContent = currentCount + 1;
+    };
+});
